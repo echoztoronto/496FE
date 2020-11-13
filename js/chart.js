c68e6207f36e2df5f0aadfca8a982f9de183f056
YW Z <32604270+echoztoronto@users.noreply.github.com>
2020-11-12 23:36:34 -0500

    chart reads from txt file now

--- js/chart.js
+++ js/chart.js
@@ -1,39 +1,77 @@
-new Chart(document.getElementById("line-chart"), {
-    type: 'line',
-    data: {
-      labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
-      datasets: [{ 
-          data: [86,114,106,106,107,111,133,221,783,2478],
-          label: "Africa",
-          borderColor: "#3e95cd",
-          fill: false
-        }, { 
-          data: [282,350,411,502,635,809,947,1402,3700,5267],
-          label: "Asia",
-          borderColor: "#8e5ea2",
-          fill: false
-        }, { 
-          data: [168,170,178,190,203,276,408,547,675,734],
-          label: "Europe",
-          borderColor: "#3cba9f",
-          fill: false
-        }, { 
-          data: [40,20,10,16,24,38,74,167,508,784],
-          label: "Latin America",
-          borderColor: "#e8c3b9",
-          fill: false
-        }, { 
-          data: [6,3,2,2,7,26,82,172,312,433],
-          label: "North America",
-          borderColor: "#c45850",
-          fill: false
-        }
-      ]
-    },
-    options: {
-      title: {
-        display: true,
-        text: 'World population per region (in millions)'
+//var data_lines = []; //global variables
+
+function processData(allText) {
+  var record_num = 10; 
+  var allTextLines = allText.split(/\r\n|\n/);
+  var entries = allTextLines[0].split(',');
+  var lines = [];
+
+  while (entries.length>0) {
+      data_set = [];
+      for (var j=0; j<record_num; j++) {
+        data_set.push(parseInt(entries.shift()));
       }
-    }
-  });
+      lines.push(data_set);
+  }
+  //alert(lines);
+  return lines;
+}
+
+
+var fs = $(document).ready(function() {
+  $.ajax({
+      type: "GET",
+      url: "data.txt",
+      dataType: "text",
+      success: function(data) {
+        var data_lines = processData(data);
+
+        var mychart = new Chart(document.getElementById("line-chart"), {
+          type: 'line',
+          data: {
+            labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
+            datasets: [{ 
+                data: data_lines[0],
+                label: "Africa",
+                borderColor: "#3e95cd",
+                fill: false
+              }, { 
+                data: data_lines[1],
+                label: "Asia",
+                borderColor: "#8e5ea2",
+                fill: false
+              }, { 
+                data: data_lines[2],
+                label: "Europe",
+                borderColor: "#3cba9f",
+                fill: false
+              }, { 
+                data: data_lines[3],
+                label: "Latin America",
+                borderColor: "#e8c3b9",
+                fill: false
+              }, { 
+                data: data_lines[4],
+                label: "North America",
+                borderColor: "#c45850",
+                fill: false
+              }
+            ]
+          },
+          options: {
+            title: {
+              display: true,
+              text: 'Some Random Graph',
+              fontSize: 20       
+            }
+          }
+        });
+      
+      mychart.update();
+      }
+   });
+});
+
+
+
+
